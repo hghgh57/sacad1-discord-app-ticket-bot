f2cfeae89f160ca62188aa108d7b38924352eeb9
hghgh57 <rizzbut3@gmail.com>
2026-09-09 14:57:48 +1000

    Update utils.js

--- discord-ticket-bot (3)/utils.js
+++ discord-ticket-bot (3)/utils.js
@@ -2,7 +2,9 @@ const { PermissionsBitField } = require("discord.js");
 const config = require("./config");
 
 function isStaff(member) {
-  return member.permissions.has(PermissionsBitField.Flags.Administrator) || member.roles.cache.has(config.bypassRole);
+  return member.permissions.has(PermissionsBitField.Flags.Administrator)
+    || member.roles.cache.has(config.bypassRole)
+    || member.roles.cache.has(config.staffRole);
 }
 
 module.exports = { isStaff };
